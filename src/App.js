9e7e98da17a37d8e53a4be5ba3679e019883b163
Sahed <ssahed65@gmail.com>
2022-04-03 22:37:16 +0600

    please commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import EVENT_HANDELER_CLASS from "./EVENT_HANDELER_CLASS/index";
+
+function App() {
+  return (
+    <div>
+      <EVENT_HANDELER_CLASS />
+    </div>
+  );
+}
+
+export default App;
