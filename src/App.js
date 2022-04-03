0e5c5778e7a795696c73ec63b78b49e2ca6f2dde
Sahed <ssahed65@gmail.com>
2022-04-03 21:36:54 +0600

    commit please

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from "react";
-import CONDITIONAL_RENDERING from "./CONDITIONAL_RENDERING/index"
-
 function App() {
   return (
-   <div>
-<CONDITIONAL_RENDERING />
-   </div>
+    <div>
+      <h2>Hello world</h2>
+    </div>
   );
 }
 
