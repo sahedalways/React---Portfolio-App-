d26bd4890c77983cfe075755745bd3b414ed42e9
Md. Sariful Islam <ssahed65@gmail.com>
2022-04-07 22:54:13 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import UseEffect from "./Component/INCREMENT_DECREMENT/IncrementDecrement";
+import React, { useEffect, useState } from 'react';
+import "./app.css";
+import Countries from './Component/Countries';
+import Search from './Component/Search';
 
 
-function App() {
-  return (
- <div>
-<UseEffect />
- </div>
-  );
+const url = "https://restcountries.com/v3.1/all";
+
+const App = () => {
+const [isLoading, setIsLoading] = useState(true);
+const [error, setError] = useState(null);
+const [countries, setCountries] = useState([]);
+const [filteredCountries, setFilteredCountries] = useState(countries);
+
+const fetchData = async(url) => {
+setIsLoading(true);
+  try {
+  const response = await fetch(url);
+  const data = await response.json()
+
+
+  setCountries(data);
+  setFilteredCountries(data);
+  setIsLoading(false);
+  setError(null);
+  console.log(countries)
+
+  }catch (error) {
+    setIsLoading(false);
+    setError(error);
+  }
+
 }
 
-export default App;
+useEffect(() => {
+fetchData(url);
+
+}, [])
+
+
+const handleRemoveCountry = (name) => {
+const filter = filteredCountries.filter((country) => country.name.common !== name )
+setFilteredCountries(filter)
+};
+
+
+const handleSearchValue = (searchValue) => {
+let value = searchValue.toLowerCase();
+const newCountries = countries.filter((country) => {
+  const countryName = country.name.common.toLowerCase();
+  return countryName.startsWith(value);
+})
+setFilteredCountries(newCountries);
+}
+
+
+  return (
+    <>
+    <h1>Country App by Sahed</h1>
+    <Search onSearch={handleSearchValue}/>
+    {isLoading && <h2>Your data is loading....</h2>}
+    {error && <h2>{error.message}</h2>}
+    <Countries countries ={filteredCountries} onRemoveCountry = {handleRemoveCountry}/>
+    </>
+  );
+};
+
+export default App
