e837eb7f7a36fcef5079cfbd0a5a06aae31bcc2a
Md. Sariful Islam <ssahed65@gmail.com>
2022-04-11 11:35:08 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,7 @@
-import React, { Component } from 'react';
-import './style.css';
+import Form from "./Form";
 
-
-class App extends Component {
-
-  intervalId = null;
-
-state = {
-  count : 0
-}
-
-handleDecrement = () => {
-  this.setState({count : this.state.count - 1})
-}
-
-handleIncrement = () => {
-  this.setState ({count: this.state.count + 1})
-}
-
-
-handleStartTimer = () => {
-  if (this.state.count > 0 && !this.intervalId) {
-    this.intervalId = setInterval (() => {
-    this.setState({count: this.state.count - 1}, () => {
-    if (this.state.count === 0) {
-    alert("Timer Finished!")
-    clearInterval(this.intervalId)
-    this.intervalId = null
-  };
-} ) ;
-
-    }, 1000)
-  };
-};
-
-
-handleStopTimer = () => {
-  if (this.intervalId) {
-    clearInterval(this.intervalId);
-    this.intervalId = null;
-  };
-};
-
-
-
-handleResetTimer = () => {
-  this.setState({count : 0});
-  clearInterval(this.intervalId);
-  this.intervalId = null;
-}
-
-
-render () {
-  return <div className='main'>
-    <h1>Simple Timer</h1>
-
-  <div className="container">
- <button className='btn' onClick={this.handleDecrement} >-</button>
-<span className='text'>{this.state.count}</span>
-<button className='btn' onClick={this.handleIncrement} >+</button>
-    </div>
-
-    <div className="container">
-  <button onClick={this.handleStartTimer} className="btn">Start</button>
-  <button onClick={this.handleResetTimer} className="btn">Reset</button>
-  <button onClick={this.handleStopTimer} className="btn">Stop</button>
-</div>
-  </div>
-}
+function App() {
+  return <Form />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+root.render(<App />);
