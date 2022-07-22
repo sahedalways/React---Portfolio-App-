6ab817fe073a9b0646a0ebeaf91b4bf02daee029
Md. Sariful Islam <ssahed65@gmail.com>
2022-07-22 17:24:42 +0600

    final codes

--- src/Components/Screens/Project_Details/ProjectDetails.jsx
+++ src/Components/Screens/Project_Details/ProjectDetails.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { allProjects } from "../../Portfolio/PortfolioData";
 import "./ProjectDetails.css";
