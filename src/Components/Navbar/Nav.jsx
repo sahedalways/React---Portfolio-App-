b6e22a7748df310c47ca9039b68f8c520bb67e30
sahedalways <ssahed65@gmail.com>
2026-08-04 11:43:55 +0600

    Make Home nav active initially; add gap on left of language toggle

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState("#");
+  const [activeNav, setActiveNav] = useState("#header");
 
   return (
     <nav>
--- src/Components/common/ThemeToggle.jsx
+++ src/Components/common/ThemeToggle.jsx
@@ -19,7 +19,7 @@ const ThemeToggle = () => {
             style={{
                 position: 'fixed',
                 top: '20px',
-                right: '88px',
+                right: '100px',
                 zIndex: 9999,
                 display: 'flex',
                 alignItems: 'center',
